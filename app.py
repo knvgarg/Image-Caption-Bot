224949636992a108a80cc7583152d8b95558dd67
knv.garg <knv.garg@gmail.com>
2021-12-26 18:41:13 +0530

    bye

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template, request
 import Caption_it as cap
 import os
+
 app = Flask(__name__)
 
-@app.route("/",methods=['GET','POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     result_dic = {}
-    dir = 'static'
+    dir = "static"
     for f in os.listdir(dir):
         os.remove(os.path.join(dir, f))
-    if request.method == 'POST':
+    if request.method == "POST":
 
-        f = request.files['userfile']
+        f = request.files["userfile"]
         path = "./static/{}".format(f.filename)
         f.save(path)
 
@@ -20,14 +22,12 @@ def index():
 
         name = f.filename
         result_dic = {
-            'image' : path,
-            'caption' : caption,
+            "image": path,
+            "caption": caption,
         }
 
-    return render_template("index.html",results=result_dic)
+    return render_template("index.html", results=result_dic)
 
 
-
-
-if __name__ == '__main__':
-    app.run(debug = True)
+if __name__ == "__main__":
+    app.run(debug=True)
